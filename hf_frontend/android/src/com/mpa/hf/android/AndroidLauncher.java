cc5a06ada33a0cd71baf9f121b8ae0768631a2e9
Matt Ahrens <matthew.ahrens@tufts.edu>
2015-12-18 23:24:02 -0500

    added default libgdx application

---
+++ hf_frontend/android/src/com/mpa/hf/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mpa.hf.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mpa.hf.HerelandFarland;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new HerelandFarland(), config);
+	}
+}
---
+++ hf_frontend/desktop/src/com/mpa/hf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mpa.hf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mpa.hf.HerelandFarland;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HerelandFarland(), config);
+	}
+}
