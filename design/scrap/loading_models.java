a605641a902f12fce715c468b5a03babfda80237
Matt Ahrens <matthew.ahrens@tufts.edu>
2015-12-19 20:19:36 -0500

    Did poppa's model loading tutorial to understand how libGDX does 3D graphics handling

---
+++ design/scrap/loading_models.java
@@ -0,0 +1,69 @@
+package com.mpa.hf;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.assets.loaders.ModelLoader;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.assets.AssetManager;
+public class HerelandFarland extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	ModelBatch modelBatch;
+    Environment environment;
+    PerspectiveCamera cam;
+    Model model;
+    ModelInstance instance;
+    CameraInputController camController;
+    AssetManager assets = new AssetManager();
+	@Override
+	public void create () {
+        modelBatch = new ModelBatch();
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(1f, 1f, 1f);
+        cam.lookAt(0,0,0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        ModelLoader<?> loader = new ObjLoader();
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+        assets.load("why2.g3db", Model.class);
+        assets.finishLoading();
+        model = assets.get("why2.g3db", Model.class);
+        instance = new ModelInstance(model);
+	}
+
+	@Override
+	public void render () {
+        camController.update();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
+	}
+
+    @Override
+    public void dispose(){
+        modelBatch.dispose();
+        model.dispose();
+    }
+}
